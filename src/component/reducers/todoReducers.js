b957efbdb2779a5aca7517e4160d772b1f0e9b9e
vedant <vedant@ssipmt.com>
2024-05-19 12:48:32 +0530

    full

--- src/component/reducers/todoReducers.js
+++ src/component/reducers/todoReducers.js
@@ -8,13 +8,13 @@ function todoReducer(state, action) {
         return [...state, { id: state.length + 1, todoData: todoText, finished: false }];
 
     }
-    else if (action.type = 'delete_todo') {
+    else if (action.type == 'delete_todo') {
         let todo = action.payload.todo;
 
         const updatelist = state.filter(t => t.id != todo.id);
         return updatelist;
     }
-    else if (action.type = 'edit_todo') {
+    else if (action.type == 'edit_todo') {
         let todo = action.payload.todo;
 
         let todoText = action.payload.todoText;
@@ -27,7 +27,7 @@ function todoReducer(state, action) {
         });
         return (updatelist);
     }
-    else if (action.type = 'finish_todo') {
+    else if (action.type == 'finish_todo') {
         let todo = action.payload.todo;
         let isFinished = action.payload.isFinished;
 
