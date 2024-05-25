5659b012af0cab680e2da8fd391e276e5a1e9b43
vedant <vedant@ssipmt.com>
2024-05-25 07:08:15 +0530

    ui 2

--- src/component/Todo/Todo.jsx
+++ src/component/Todo/Todo.jsx
@@ -6,21 +6,24 @@ function Todo({todoData, isFinished, changeFinished, onDelete, onEdit}){
     const [editText,setEditText]= useState(todoData);
 
     return(
-        <div>
-            <input type="checkbox" onChange={(e)=> {
+        <div className="flex flex-col gap-4">
+            <div className="mt-3">
+            <input className="rounded-full mx-3" type="checkbox" onChange={(e)=> {
                 console.log(e.target.checked) 
                 setFinished(e.target.checked) 
                 changeFinished(e.target.checked)
                 }} />
-            {(isEditting) ? <input type="text" value={editText} onChange={e => setEditText(e.target.value)}/> : <span>{todoData}</span> }
-            <button onClick={()=>{
+            {(isEditting) ? <input className="w-fit px-4 py-1 rounded-md"  type="text" value={editText} onChange={e => setEditText(e.target.value)}/> : <span className="px-4 py-1 rounded-md bg-white text-black " >{todoData}</span> }
+
+            <button className="px-4 py-1 mx-2 rounded-md bg-green-500 text-black " onClick={()=>{
                 setIsEditting(!isEditting)
                 onEdit(editText)
             }}> 
                 {(isEditting)? 'Save' : 'Edit'}
             </button>
 
-            <button onClick = {onDelete} > Delete </button>
+            <button className="px-4 py-1 rounded-md bg-red-700 text-white mx-2" onClick = {onDelete} > Delete </button>
+            </div>
         </div>
     )
 }
